1d05f5872d4155a324663f9338864f5c7d6ca176
Yusif Bayramov <byusub2@gmail.com>
2022-05-29 01:33:45 +0400

    scroll edit

--- js/about.js
+++ js/about.js
@@ -10,3 +10,30 @@ btnr.addEventListener("click", () => {
     }
 
 });
+
+window.addEventListener("scroll", () => {
+    let scroll = document.querySelector(".about-c");
+    let cposition = scroll.getBoundingClientRect().top;
+    let sposition = window.innerHeight / 1;
+    if (cposition < sposition) {
+        scroll.style.opacity = "1";
+        scroll.style.transform = "scale(1)";
+    } else {
+        scroll.style.opacity = "0";
+        scroll.style.transform = "scale(0.1)";
+    }
+});
+
+
+window.addEventListener("scroll", () => {
+    let skils = document.querySelector(".wrapper");
+    let cposition = skils.getBoundingClientRect().top;
+    let sposition = window.innerHeight / 1;
+    if (cposition < sposition) {
+        skils.style.opacity = "1";
+        skils.style.transform = "scale(1)";
+    } else {
+        skils.style.opacity = "0";
+        skils.style.transform = "scale(0.1)";
+    }
+});
