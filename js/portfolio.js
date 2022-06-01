1ab3936098ccf2095f1b97d26c4197bc981a7f02
Yusif Bayramov <byusub2@gmail.com>
2022-06-01 23:09:04 +0400

    new is

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,3 +13,17 @@ window.addEventListener("scroll", () => {
         skils.style.transform = "scale(0.1)";
     }
 });
+
+window.addEventListener("scroll", () => {
+    let skils = document.querySelector(".scrl2");
+    let cposition = skils.getBoundingClientRect().top;
+    let sposition = window.innerHeight / 1;
+    if (cposition < sposition) {
+
+        skils.style.opacity = "1";
+        skils.style.transform = "scale(1)";
+    } else {
+        skils.style.opacity = "0";
+        skils.style.transform = "scale(0.1)";
+    }
+});
