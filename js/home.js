dc747e91b5bb47015cf27e5572046509d3963ed2
Yusif Bayramov <byusub2@gmail.com>
2022-06-10 19:13:35 +0400

    lefticon

--- js/home.js
+++ js/home.js
@@ -2,3 +2,17 @@ const spiner = document.querySelector(".spiner");
 setInterval(() => {
     spiner.style.display = "none"
 }, 2000);
+
+window.addEventListener("scroll", () => {
+    let lefticon = document.querySelector(".lefti-c")
+    let scroll = document.querySelector("footer");
+    let cposition = scroll.getBoundingClientRect().top;
+    let sposition = window.innerHeight / 1;
+    if (cposition < sposition) {
+        lefticon.style.opacity = "0";
+
+    } else {
+        lefticon.style.opacity = "1";
+
+    }
+});
