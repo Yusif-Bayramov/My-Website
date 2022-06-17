a0ba425ed56a6e7c11208ee078ed6df2fc555fea
Yusif Bayramov <byusub2@gmail.com>
2022-06-18 01:39:39 +0400

    input textarea edit

--- js/contact.js
+++ js/contact.js
@@ -13,3 +13,32 @@ window.addEventListener("scroll", () => {
         skils.style.transform = "scale(0.1)";
     };
 });
+
+let label = document.querySelector(".labeli");
+let label2 = document.querySelector(".labeli2");
+let input = document.querySelector("input");
+let textarea = document.querySelector("textarea");
+
+input.addEventListener("click", () => {
+    if (label.style.transform == "translateY(-26px)") {
+        label.style.transform = "translateY(0)";
+        label.style.fontSize = "1.4rem";
+    } else {
+        label2.style.transform = "translateY(0)";
+        label.style.transform = "translateY(-26px)";
+        label.style.fontSize = "1.2rem";
+        label2.style.fontSize = "1.4rem";
+    }
+});
+
+textarea.addEventListener("click", () => {
+    if (label2.style.transform == "translateY(-26px)") {
+        label.style.fontSize = "1.4rem";
+        label2.style.transform = "translateY(0)";
+    } else {
+        label.style.fontSize = "1.4rem";
+        label.style.transform = "translateY(0)";
+        label2.style.transform = "translateY(-26px)";
+        label2.style.fontSize = "1.2rem";
+    }
+});
